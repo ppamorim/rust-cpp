5f045aa2b1ee98379fbe83d982bd35de0039fc42
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2019-09-14 16:29:22 +0100

    Removing lib

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
-extern crate cpp_build;
+// extern crate cpp_build;
 
 fn main() {
-    let include_path = "/Users/ppamorim/Repository/rust-cpp/src/lib";
+    // let include_path = "/Users/ppamorim/Repository/rust-cpp/src/lib";
     let lib_path = "/Users/ppamorim/Repository/rust-cpp/src/lib";
-    cpp_build::Config::new().include(include_path).build("src/lib.rs");
+    // cpp_build::Config::new().include(include_path).build("src/lib.rs");
     println!("cargo:rustc-link-search=native={}", lib_path);
     println!("cargo:rustc-link-lib=static=sum");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
-#[macro_use]
-extern crate cpp;
+// #[macro_use]
+// extern crate cpp;
 
-cpp!{{
-    #include <iostream>
-}}
+// cpp!{{
+//     #include <iostream>
+// }}
 
-pub fn foo() {
-    let name = std::ffi::CString::new("World").unwrap();
-    let name_ptr = name.as_ptr();
-    let r = unsafe {
-        cpp!([name_ptr as "const char *"] -> u32 as "int32_t" {
-            std::cout << "Hello, " << name_ptr << std::endl;
-            return 42;
-        })
-    };
-    assert_eq!(r, 42)
-}
+// pub fn foo() {
+//     let name = std::ffi::CString::new("World").unwrap();
+//     let name_ptr = name.as_ptr();
+//     let r = unsafe {
+//         cpp!([name_ptr as "const char *"] -> u32 as "int32_t" {
+//             std::cout << "Hello, " << name_ptr << std::endl;
+//             return 42;
+//         })
+//     };
+//     assert_eq!(r, 42)
+// }
