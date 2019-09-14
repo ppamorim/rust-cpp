118bee16fa9da7aa378b06f5de8e65a5d9aff2ea
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2019-09-14 20:32:53 +0100

    Remove absolute path

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
-// extern crate cpp_build;
+extern crate cpp_build;
+use std::env::var;
 
 fn main() {
-    let include_path = "/Users/ppamorim/Repository/rust-cpp/src/lib";
-    let lib_path = "/Users/ppamorim/Repository/rust-cpp/src/lib";
+    let manifest_dir = var("CARGO_MANIFEST_DIR").unwrap();
+    let include_path = format!("{}/src/lib", manifest_dir);
+    let lib_path = format!("{}/src/lib", manifest_dir);
     cpp_build::Config::new().include(include_path).build("src/lib.rs");
     println!("cargo:rustc-link-search=native={}", lib_path);
     println!("cargo:rustc-link-lib=static=sum");
